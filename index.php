e5d770a66522bbe9dead876980e2ad6f0d1a5e8c
dd310 <dd310@exeter.ac.uk>
2017-09-06 11:41:22 +0100

    changed response

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
     $search = $_GET["q"];
     if ($search != ""){
-        echo "some query";
+        echo '{"messages":[
+            {"text: "You searched for'.$search.'"}
+        ]}';
     } else {
         echo "empty response";
     }
