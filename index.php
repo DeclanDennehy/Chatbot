5d64663e9c2c000a9b3f3845bd6b7aecac35ea22
dd310 <dd310@exeter.ac.uk>
2017-09-06 15:49:33 +0100

    successfully showing all results names

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
 <?php
+    const RESPONSE_START = '{"messages": [';
+    const RESPONSE_END = ']}';
+    const TEXT_START = '{"text": "';
+    const TEXT_END_COMMA = '"},';
+    const TEXT_END = '"}';
+
     if(isset($_GET["q"])){
         // untested BEGIN
         
         $results = json_decode(file_get_contents("https://swapi.co/api/people?search=".$_GET["q"]),TRUE);
-        echo '{"messages":[{"text": "Found '.$results["count"].' result(s)!"}]}';
-        echo '{"messages":[{"text": "More info?"}]}';
-        // untested END
+        $count = $results["count"];
+        $message = RESPONSE_START;
+        for($i = 0; $i < $count-1; $i++){
+            $message .= TEXT_START;
+            $message .= $results["results"][$i]["name"];
+            $message .= TEXT_END_COMMA;
+        }
+        $message .= TEXT_START;
+        $message .= $results["results"][$i]["name"];
+        $message .= TEXT_END;
+        $message .= RESPONSE_END;
+        echo $message;
     }
     else{
-        echo '{"messages":[{"text": "No query was given!"}]}';
+        echo RESPONSE_START.TEXT_START.'No query was given!'.TEXT_END.RESPONSE_END;
     }
 ?>
