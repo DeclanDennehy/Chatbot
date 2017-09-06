b384e3817d6cb49a324db1e1729677d0a6b645a8
dd310 <dd310@exeter.ac.uk>
2017-09-06 12:00:12 +0100

    added block template

--- index.php
+++ index.php
@@ -1,7 +1,30 @@
 <?php
     $search = $_GET["q"];
     if ($search != ""){
-        echo '{"messages": [{"text": "Welcome to our store!"},{"text": "How can I help you?"}]}';
+        $response_json = file_get_contents("http://swapi.co/api/people/?search=".$search);
+        $response_parsed = json_decode($response_json, TRUE);
+        echo '{"messages": [{"text": "Found '.$response_parsed["count"].' possibilities"}]}';
+        echo '{
+  "messages": [
+    {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "button",
+          "text": "Hello!",
+          "buttons": [
+            {
+              "type": "show_block",
+              "block_name": "some block name",
+              "title": "Show the block!"
+            }
+          ]
+        }
+      }
+    }
+  ]
+}';
+        
     } else {
         echo '{"messages": [{"text": "No response found!"}]}';
     }
