1a32f34911d1f1d7b4c016a4aae449357ec998f3
dd310 <dd310@exeter.ac.uk>
2017-09-06 16:25:03 +0100

    up to 10 options now available

--- index.php
+++ index.php
@@ -10,32 +10,22 @@
         
         $results = json_decode(file_get_contents("https://swapi.co/api/people?search=".$_GET["q"]),TRUE);
         $count = $results["count"];
-        $message = RESPONSE_START;
+        $message = '{"messages": [{"text":  "Who are you interested in?","quick_replies": ['; 
+        
         for($i = 0; ($i < $count-1 && $i < 9); $i++){
-            $message .= TEXT_START;
-            $message .= $results["results"][$i]["name"];
-            $message .= TEXT_END_COMMA;
+            $message .= '{
+                  "title":"'.$results["results"][$i]["name"].'",
+                  "block_names":["person"],
+                  "set_attributes": {"person_chosen": "'.$results["results"][$i]["url"].'"}
+                },';
         }
-        $message .= TEXT_START;
-        $message .= $results["results"][$i]["name"];
-        $message .= TEXT_END;
-        $message .= RESPONSE_END;
-        //echo $message;
-        echo '{"messages": [{
-            "text":  "Who are you interested in?",
-            "quick_replies": [
-                {
-                  "title":"'.$results["results"][0]["name"].'",
+        $message .= '{
+                  "title":"'.$results["results"][$i]["name"].'",
                   "block_names":["person"],
-                  "set_attributes": {"person_chosen": "'.$results["results"][0]["url"].'"}
-                },
-                {
-                  "title":"'.$results["results"][1]["name"].'",
-                  "block_names":["person"],
-                  "set_attributes": {"person_chosen": "'.$results["results"][1]["url"].'"}
-                }
-            ]
+                  "set_attributes": {"person_chosen": "'.$results["results"][$i]["url"].'"}
+                }]
         }]}';
+        echo $message;
     }
     else{
         echo RESPONSE_START.TEXT_START.'No query was given!'.TEXT_END.RESPONSE_END;
