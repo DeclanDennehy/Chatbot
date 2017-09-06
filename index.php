91a3726db80b3ea87dd2824087cae995a59988b3
dd310 <dd310@exeter.ac.uk>
2017-09-06 12:12:30 +0100

    added detail to response

--- index.php
+++ index.php
@@ -3,25 +3,21 @@
     if ($search != ""){
         $response_json = file_get_contents("http://swapi.co/api/people/?search=".$search);
         $response_parsed = json_decode($response_json, TRUE);
+        $name = $response_parsed["results"][0]["name"];
+        $yob = $response_parsed["results"][0]["birth_year"];
+        $species = $response_parsed["results"][0]["species"][0];
+        $homeworld = $response_parsed["results"][0]["homeworld"];
+        $films = implode(',',$response_parsed["results"][0]["films"]);
         echo '{
   "messages": [
     {"text": "Found '.$response_parsed["count"].' possibilities"},
-    {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "button",
-          "text": "Hello!",
-          "buttons": [
-            {
-              "type": "show_block",
-              "block_name": "some block name",
-              "title": "Show the block!"
-            }
-          ]
-        }
-      }
-    }
+    {"text": "
+        Name: '.$name.'\n
+        Year of Birth: '.$yob.'\n
+        Species: '.$species.'
+        Homeworld: '.$homeworld.'
+        Appears in: '.$films.'
+    "} 
   ]
 }';
         
