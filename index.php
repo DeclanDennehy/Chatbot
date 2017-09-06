0c7878cf2eecb0999b775d1ff0e2db53ad244211
dd310 <dd310@exeter.ac.uk>
2017-09-06 15:20:41 +0100

    restart now have check working

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php
     if(isset($_GET["q"])){
-        echo '
-        {"messages":[
-            {"text": "Query was given!"}
-        ]}';
+        // untested BEGIN
+        
+        $results = json_decode(file_get_contents("https://swapi.co/api/people?search=".$_GET["q"]),TRUE);
+        echo '{"messages":[{"text": "Found '.$results["count"].' result(s)!"}]}';
+        echo '{"messages":[{"text": "More info?"}]}';
+        // untested END
     }
     else{
-        echo '
-        {
-            "messages":[
-                {"text": "No query was given!"}
-            ]
-        }';
+        echo '{"messages":[{"text": "No query was given!"}]}';
     }
 ?>
