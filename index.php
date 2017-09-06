bb91b378f43edfb8df7380554bb66ac2a9a44166
dd310 <dd310@exeter.ac.uk>
2017-09-06 13:02:21 +0100

    now should show multiple results

--- index.php
+++ index.php
@@ -1,21 +1,45 @@
 <?php
     $search = $_GET["q"];
+    // create empty response to be added to
+    $message = "";
+    
+    // empty query check, note that this should never actually occur since we are just using facebook messenger which handles that on their end
     if ($search != ""){
+        // send request to API
         $response_json = file_get_contents("http://swapi.co/api/people/?search=".$search);
         $response_parsed = json_decode($response_json, TRUE);
-        $name = $response_parsed["results"][0]["name"];
-        $yob = $response_parsed["results"][0]["birth_year"];
-        $species = $response_parsed["results"][0]["species"][0];
-        $homeworld = $response_parsed["results"][0]["homeworld"];
-        $films = implode(',',$response_parsed["results"][0]["films"]);
-        echo '{
-  "messages": [
-    {"text": "Found '.$response_parsed["count"].' possibilities"},
-    {"text": "Name: '.$name.'\\nYear of Birth: '.$yob.'\\nSpecies: '.$species.'\\nHomeworld: '.$homeworld.'\\nAppears in: '.$films.'"} 
-  ]
-}';
         
+        $count = $response_parsed["count"];
+        
+        if ($count > 0){
+            $message += '{"messages": [';
+            // add note to show how many are found only show max of 5 to prevent spamming
+            if ($count < 5){
+                $message += '{"text": "Found "'.$count.'"},';
+            }
+            else {
+                $message += '{"text": "Found '.$count.', showing 5"},';
+            }
+            
+            // for every result up to 5
+            for($i = 0; ($i < $count && $i < 5); $i++){
+                $message += '{"text": "Name: '.$count = $response_parsed["results"][$i]["name"].'\\n';
+                $message += 'Year of Birth: '.$count = $response_parsed["results"][$i]["birth_year"].'\\n';
+                $homeplanet_request = json_decode(file_get_contents($response_parsed["results"][$i]["homeworld"]),TRUE);
+                $message += 'Homeworld: '.$homeplanet_request["name"].'\\n';
+                $species_request = json_decode(file_get_contents($response_parsed["results"][$i]["species"]),TRUE);
+                $message += 'Species: '.$species_request["name"].'\\n';
+                $message += '"},';
+            }
+            
+            // remove the last comma added by the for loop
+            $message = substr($message,0,-1);
+            $message += ']}';
+        } else{
+            $message += '{"messages": [{"text": "No response found!"}]}'; 
+        }
     } else {
-        echo '{"messages": [{"text": "No response found!"}]}';
+        $message += '{"messages": [{"text": "No response found!"}]}';
     }
+    echo $message;
 ?>
