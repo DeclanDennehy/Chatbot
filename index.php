985515f1d1295514a11da4ec6e993a5c06735115
dd310 <dd310@exeter.ac.uk>
2017-09-06 17:31:06 +0100

    testing with neater wording

--- index.php
+++ index.php
@@ -28,6 +28,6 @@
     if(isset($_GET["url"])){
         $results = json_decode(file_get_contents($_GET["url"]),TRUE);
         $homeworld = json_decode(file_get_contents($results["homeworld"]),TRUE);
-        echo '{"messages": [{"text": "'.$results["name"].$results["gender"].$results["height"].$results["mass"].$homeworld["name"].'"}]}';
+        echo '{"messages": [{"text": "Name: '.$results["name"].', Gender: '.$results["gender"].', Height: '.$results["height"].'cm, Weight '.$results["mass"].'kg, Homeworld: '.$homeworld["name"].'"}]}';
     }
 ?>
