d77ff8d518693f21c0722a7762dee37c5165ac98
dd310 <dd310@exeter.ac.uk>
2017-09-07 22:58:25 +0100

    Cleaned interface and testing redesign of person

--- index.php
+++ index.php
@@ -1,95 +1,87 @@
 <?php
-    // handle person querys
-    if(isset($_GET["query_person"])){
-        include 'person.php';
-        person_query();
-        return;
-    }
-
-    // handle person urls
-    if(isset($_GET["url_person"])){
-        include 'person.php';
-        person_description();
-        return;
-    }
-
-
-
-    // handle planet querys
-    if(isset($_GET["query_planet"])){
-        include 'planet.php';
-        planet_query();
-        return;
-    }
-
-    // handle planet urls
-    if(isset($_GET["url_planet"])){
-        include 'planet.php';
-        planet_description();
-        return;
-    }
-
-
-
-     // handle species querys
-    if(isset($_GET["query_species"])){
-        include 'species.php';
-        species_query();
-        return;
-    }
-
-    // handle species urls
-    if(isset($_GET["url_species"])){
-        include 'species.php';
-        species_description();
-        return;
-    }
-
-
-
-    // handle species starship querys
-    if(isset($_GET["query_starships"])){
-        include 'starships.php';
-        starships_query();
-        return;
-    }
+    // retrive the only GET attribute
+    $get_attribute = array_keys($_GET)[0];
     
-    // handle species starship querys
-    if(isset($_GET["url_starships"])){
-        include 'starships.php';
-        starship_description();
-        return;
-    }
+    // swtich statement to check the key of GET attribute and then execute the corresponding function
+    // include statements are done seperaely in each case so that files are only included when necessary
+    switch ($get_attribute){
+            
+        // search for a person and send the results to the chatbot
+        case "query_person":
+            include 'person.php';
+            person_query();
+            break;
 
-
-
-    // handle species vehicle querys
-    if(isset($_GET["query_vehicles"])){
-        include 'vehicles.php';
-        vehicles_query();
-        return;
-    }
-    
-    // handle species vehicle querys
-    if(isset($_GET["url_vehicles"])){
-        include 'vehicles.php';
-        vehicle_description();
-        return;
-    }
-
-
-
-    // handle species films querys
-    if(isset($_GET["query_films"])){
-        include 'films.php';
-        films_query();
-        return;
-    }
-
-    // handle species films querys
-    if(isset($_GET["url_films"])){
-        include 'films.php';
-        film_description();
-        return;
+        // get the description of a person and send the results to the chatbot
+        case "url_person":
+            include 'person.php';
+            person_description();
+            break;
+        
+        // search for a planet and send the results to the chatbot
+        case "query_planet":
+            include 'planet.php';
+            planet_query();
+            break;
+        
+        // get the description of a planet and send the results to the chatbot
+        case "url_planet":
+            include 'planet.php';
+            planet_description();
+            break;
+            
+        // search for a species and send the results to the chatbot
+        case "query_species":
+            include 'species.php';
+            species_query();
+            break;
+            
+        // get the description of a species and send the results to the chatbot
+        case "url_species":
+            include 'species.php';
+            species_description();
+            break;
+            
+        // search for a starship and send the results to the chatbot
+        case "query_starships":
+            include 'starships.php';
+            starships_query();
+            break;
+            
+        // get the description of a starship and send the results to the chatbot
+        case "url_starships":
+            include 'starships.php';
+            starship_description();
+            break;
+            
+        // search for a vehicle and send the results to the chatbot
+        case "query_vehicles":
+            include 'vehicles.php';
+            vehicles_query();
+            break;
+        
+        // get the description of a vehicle and send the results to the chatbot
+        case "url_vehicles":
+            include 'vehicles.php';
+            vehicle_description();
+            break;
+            
+        // search for a film and send the results to the chatbot
+        case "query_films":
+            include 'films.php';
+            films_query();
+            break;
+            
+        // get the description of a film and send the results to the chatbot
+        case "url_films":
+            include 'films.php';
+            film_description();
+            break;
+            
+        // in the default case then return with a issue message - note that due to being sent to  
+        // the chatbot directly a friendly essentially useless message is sent
+        default:
+            echo '{"messages":[{"text": "Oops! Something has gone wrong with the server!"}]}';
+            break;
     }
 ?>
--- person.php
+++ person.php
@@ -1,22 +1,48 @@
 <?php
+    /*
+     *  Function to send a search request to the SWAPI and then send the results in a chatfuel compatabile format
+     *  If no results are given say no results were found and then try again.
+     */
     function person_query(){
-        $results = json_decode(file_get_contents("https://swapi.co/api/people?search=".$_GET["query_person"]),TRUE);
-        $count = $results["count"];
-        $message = '{"messages": [{"text":  "Who are you interested in?","quick_replies": ['; 
+        $search = $_GET["query_person"];
         
-        for($i = 0; ($i < $count-1 && $i < 9); $i++){
+        // format the string to allow form multiple words
+        $search = str_replace("%20", ",", $search);
+        $search = str_replace(" ",",",$search);
+        
+        // send the request to API and then convert it into a PHP array
+        $results = json_decode(file_get_contents("https://swapi.co/api/people?search=".$search),TRUE);
+        
+        // get the number of results
+        $count = $results["count"];
+        
+        if ($count > 0){
+            // build start of message
+            $message = '{"messages": [{"text":  "Who are you interested in?","quick_replies": ['; 
+
+            // loop through all results except the last and add their contents to the message
+            for($i = 0; ($i < $count-1 && $i < 9); $i++){
+                $message .= '{
+                      "title":"'.$results["results"][$i]["name"].'",
+                      "block_names":["Person"],
+                      "set_attributes": {"person_chosen": "'.$results["results"][$i]["url"].'"}
+                    },';
+            }
+            
+            // add the contents of the last result and end the message. 
             $message .= '{
-                  "title":"'.$results["results"][$i]["name"].'",
-                  "block_names":["Person"],
-                  "set_attributes": {"person_chosen": "'.$results["results"][$i]["url"].'"}
-                },';
+                      "title":"'.$results["results"][$i]["name"].'",
+                      "block_names":["Person"],
+                      "set_attributes": {"person_chosen": "'.$results["results"][$i]["url"].'"}
+                    }]
+            }]}';
+        } else {
+            $message = '
+            {
+                "messages": [{"text": "No results are found"}],
+                "redirect_to_blocks": ["Query Person"]
+            }';
         }
-        $message .= '{
-                  "title":"'.$results["results"][$i]["name"].'",
-                  "block_names":["Person"],
-                  "set_attributes": {"person_chosen": "'.$results["results"][$i]["url"].'"}
-                }]
-        }]}';
         echo $message;
         return;
     }
